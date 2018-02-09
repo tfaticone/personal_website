3cb94b369903c14a2566fd8c4ad1b6de4b4c1403
Thomas Faticone <tjf4881@rit.edu>
2018-02-08 22:47:16 -0500

    Initial website commit

---
+++ swen344/get_target.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1>Get results</h1>
+<div>Name: <?php echo $_GET['fname'] . ' ' . $_GET['lname'] ?></div>
+<div>Age: <?php echo $_GET['age'] ?></div>
+<div>State: <?php echo $_GET['state'] ?></div>
+<div>Gender: <?php echo $_GET['gender'] ?></div>
+
+</body>
+</html>
---
+++ swen344/post_target.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1>Get results</h1>
+<div>Name: <?php echo $_POST['fname'] . ' ' . $_POST['lname'] ?></div>
+<div>Age: <?php echo $_POST['age'] ?></div>
+<div>State: <?php echo $_POST['state'] ?></div>
+<div>Gender: <?php echo $_POST['gender'] ?></div>
+
+</body>
+</html>
