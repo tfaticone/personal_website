880ca88fe6da82a7ca62be0fa36ef96f89d286b1
Thomas Faticone <tjf4881@rit.edu>
2018-02-16 10:48:38 -0500

    Added in weather information

--- swen344/REST/RestClient-weather.php
+++ swen344/REST/RestClient-weather.php
@@ -10,12 +10,17 @@
 
     $weather_list=file_get_contents('http://www.se.rit.edu/~swen-344/activities/rest/RESTAPI-Weather.php?action=get_weather_list&secret_key=' . $key_info['key'] . '&id=' . $key_info['id']);
     $weather_list=json_decode($weather_list, true);
+
+    if($_GET['zip']) {
+        $weather=file_get_contents('http://www.se.rit.edu/~swen-344/activities/rest/RESTAPI-Weather.php?action=get_weather&secret_key=' . $key_info['key'] . '&id=' . $key_info['id'] . '&zip=' . $_GET['zip']);
+        $weather=json_decode($weather_list, true);
+    }
     ?>
 
     <h1>Choose a weather zip code from the dropdown</h1>
     <form action="#" method="get">
         Zip code: <br />
-        <select>
+        <select name="zip">
             <?php foreach ($weather_list as $place): ?>
                 <option value="<?php echo $place['zip'] ?>"><?php echo $place['zip'] ?></option>
             <?php endforeach; ?>
@@ -23,12 +28,23 @@
         <input type="submit" value="Submit">
     </form>
 
+<?php if ($_GET['zip']): ?>
     <table border ="1">
         <tr>
-            <td>Name: </td><td> <?php echo "hi" ?></td>
+            <td>Zip: </td><td> <?php echo $weather["zip"] ?></td>
+        </tr>
+        <tr>
+            <td>Name: </td><td> <?php echo $weather["name"] ?></td>
+        </tr>
+        <tr>
+            <td>Forecast: </td><td> <?php echo $weather["forecast"] ?></td>
+        </tr>
+        <tr>
+            <td>Image </td><td><img src="<?php echo $weather["image"] ?>" /></td>
         </tr>
     </table>
-    <br />
+
+<?php endif; ?>
 
 </body>
 </html>
